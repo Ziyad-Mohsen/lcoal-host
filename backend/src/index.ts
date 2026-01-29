bb0d2c65a539b3e42b66c939a0db5352a0c58788
Ziad Mohsen <zm.ze05ad@gmail.com>
2026-01-29 21:50:55 +0200

    :sparkles: feat: dynamically  get local ip to use in network host

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,4 @@
+import os from "os";
 import express, {
   type Application,
   type Request,
@@ -7,7 +8,6 @@ import cors from "cors";
 import filesRouter from "./modules/files/files.routes.ts";
 
 const app: Application = express();
-const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
 app.use(express.json());
 app.use(cors());
@@ -18,6 +18,30 @@ app.get("/api/v1", (_req: Request, res: Response) => {
   res.json({ message: "hello world" });
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+const getLocalIpAddress = () => {
+  const networkInterfaces = os.networkInterfaces();
+
+  for (const interfaceName in networkInterfaces) {
+    const interfaces = networkInterfaces[interfaceName];
+    for (const iface of interfaces || []) {
+      // Check for IPv4, not internal (loopback), and within the 192.168 range
+      if (
+        iface.family === "IPv4" &&
+        !iface.internal &&
+        iface.address.startsWith("192.168")
+      ) {
+        return iface.address;
+      }
+    }
+  }
+  return "127.0.0.1 (Local IP not found)";
+};
+
+const myIp = getLocalIpAddress();
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on:`);
+  console.log(`Local: http://localhost:${port}`);
+  console.log(`Network: http://${myIp}:${port}`);
 });
