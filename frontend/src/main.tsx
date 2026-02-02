8c72b99308e1ce7ee5766ba2d71cb3f19cf8d985
Ziad Mohsen <zm.ze05ad@gmail.com>
2026-02-02 23:42:23 +0200

    :sparkles: feat(frontend): setup tanstack query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,17 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import { ThemeProvider } from "./components/theme-provider.tsx";
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "./lib/queryClient.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 );
